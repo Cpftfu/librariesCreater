90218a7e95e6a0b81d1dd7061f14672d8b40e316
Cpftfu <alexseev45s@gmail.com>
2024-06-07 22:35:36 +0300

    prack 8 is DONE

---
+++ forThemesPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryCreateWPF
+{
+	/// <summary>
+	/// Логика взаимодействия для forThemesPage.xaml
+	/// </summary>
+	public partial class forThemesPage : Window
+	{
+		public forThemesPage()
+		{
+			InitializeComponent();
+		}
+
+		private void for_romantic_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void for_forest_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void for_technological_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void for_vintage_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void for_space_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			startPage window = new startPage();
+			window.Show();
+			Close();
+		}
+	}
+}
---
+++ startPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryCreateWPF
+{
+	/// <summary>
+	/// Логика взаимодействия для startPage.xaml
+	/// </summary>
+	public partial class startPage : Window
+	{
+		public startPage()
+		{
+			InitializeComponent();
+		}
+
+		private void for_themes_Click(object sender, RoutedEventArgs e)
+		{
+			forThemesPage window1 = new forThemesPage();
+			window1.Show();
+			Close();
+        }
+
+		private void for_de_cerialization_Click(object sender, RoutedEventArgs e)
+		{
+			forDeCerializationPage window2 = new forDeCerializationPage();
+			window2.Show(); 
+			Close();
+		}
+	}
+}
